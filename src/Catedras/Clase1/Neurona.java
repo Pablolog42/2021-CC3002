41ef1a9d119e3a5d1abf00bf67d7d95bec5b0d3d
Pablo González <pablolog42@gmail.com>
2021-08-18 18:47:59 -0400

    Clase 1

---
+++ src/Catedras/Clase1/Neurona.java
@@ -0,0 +1,26 @@
+package src.Catedras.Clase1;
+
+public class Neurona {
+	private double weight1,weight2;
+	private double bias;
+
+	// Constructor de la clase, no devuelve ningun valor, y tiene el mismo nombre que la clase
+	public Neurona(double w1, double w2, double b){
+		weight1 = w1;
+		weight2 = w2;
+		bias = b;
+	}
+
+	// Metodo que devuelve un doble
+	public double computeZ(double input1, double input2){
+		return input1 * weight1 +
+		       input2 * weight2 +
+		       bias;
+	}
+
+	// this <=> self
+
+public double feed(double input1,double input2) {
+	return bias;
+}
+}
---
+++ src/Catedras/Clase1/NeuronaEjemplo.java
@@ -0,0 +1,7 @@
+package src.Catedras.Clase1;
+
+public class NeuronaEjemplo {
+	public static void main(String[] args){
+
+	}
+}
